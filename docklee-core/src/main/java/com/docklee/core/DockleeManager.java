d58c2c1d5dd3da4bfa4f8065d17d1afb53b6131f
agsn10 <agsn10@hotmail.com>
2022-03-01 22:19:07 -0300

    Docklee Multi-Module - code removed

--- docklee-core/src/main/java/com/docklee/core/DockleeManager.java
+++ docklee-core/src/main/java/com/docklee/core/DockleeManager.java
@@ -74,6 +74,7 @@ public class DockleeManager extends AbstractDockleeManager{
      * @throws
      */
     private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
         String action = orchestrator.getAction(req, resp);
         orchestrator.executeAction(action);
     }
