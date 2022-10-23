95a3902a57b6c3725ebe8948faf292f7661af67d
agsn10 <agsn10@hotmail.com>
2022-10-23 10:38:35 -0300

    Add logo and update main layout

---
+++ docklee-ui/src/main/java/com/docklee/ui/resource/component/InfoComponent.java
@@ -0,0 +1,12 @@
+package com.docklee.ui.resource.component;
+
+public class InfoComponent implements Component{
+
+    /**
+     * @return
+     */
+    @Override
+    public String create() {
+        return null;
+    }
+}
